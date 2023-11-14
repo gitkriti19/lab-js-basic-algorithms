0c72056b0db8fc83d315e667d83a2470d0fa9446
Kriti Arora <kriti11.arora@gmail.com>
2023-11-14 15:01:38 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+let hacker1 = "kriti";
+console.log("The driveer's name is "+hacker1);
+let hacker2 = "also kriti";
+console.log("The navigators name is "+hacker2);
 
+// Iteration 2: Conditionals
+let d = hacker1.length;
+let d2 = hacker2.length;
+if(d>d2) console.log("drivers name is longer it has "+ d+" characters");
+else if(d2>d) console.log("navigators name is longer it has "+d2+" characters");
+else console.log("they have equal number of characters");
 
 // Iteration 3: Loops
+
+//system check
+console.log("I'm ready");
